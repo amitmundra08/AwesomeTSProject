88423a86ea73c991d3e5766fe5c1b97f3df3bf62
Amit Mundra <amitmundra@Amits-MacBook-Air.local>
2020-03-19 09:34:48 +0530

    adding data file

--- src/data-sources/videoData.ts
+++ src/data-sources/videoData.ts
@@ -5,41 +5,7 @@ export const videoData = {
      videosArray: [
         {
             url: samplevideo,
-            comments: [
-                {
-                    name: 'Amit',
-                    comment: 'Hello there',
-                    id: '1',
-                    date: 1584565345678
-                }, {
-                    name: 'Ayush',
-                    comment: 'Hii Here',
-                    id: '2',
-                    date: 1576702618212
-                },
-                {
-                    name: 'Amit',
-                    comment: 'Hello there',
-                    id: '3',
-                    date: 1584565345678
-                }, {
-                    name: 'Ayush',
-                    comment: 'Hii Here',
-                    id: '4',
-                    date: 1576702618212
-                },
-                {
-                    name: 'Amit',
-                    comment: 'Hello there',
-                    id: '5',
-                    date: 1584565345678
-                }, {
-                    name: 'Ayush',
-                    comment: 'Hii Here Here Here Here Here Hii Here Here Here Here Here',
-                    id: '6',
-                    date: 1576702618212
-                }
-            ]
+            comments: []
         },
         {
             url: secondSampleVideo,
